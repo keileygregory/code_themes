4c4070de847f4c81bcabf823ca9718be381b9827
keileygregory <kkeiilinicole@gmail.com>
2026-07-09 11:59:38 -0400

    add to better comments vscode extension

---
+++ bettercomments_examples.py
@@ -0,0 +1,47 @@
+# MY CUSTOM COLORS
+#༚ text 
+#⋆ text 
+#༝ text 
+#¸ text 
+#‧ text 
+#⋄ text 
+#˚ text 
+#˖ text 
+#· text 
+#! text 
+#𖠜 text 
+#ㅱ text 
+#˓ text 
+
+# BRIGHT AQUAS AND BLUES
+#✦ text 
+#❈ text 
+#⟣ text 
+#❖ text 
+#◈ text 
+#⁘ text
+
+# ROSE AND BLUSH
+#❥ text 
+#✿ text 
+#❀ text 
+#⟡ text 
+#◜ text 
+#⊹ text 
+#✎ text 
+
+# SEAFOAM AND TEAL
+#≈ text 
+#⌇ text 
+#❧ text 
+#⟢ text 
+#◦ text 
+#~ text 
+
+# LAVENDER AND PERIWINKLE
+#❋ text 
+#✧ text 
+#⟐ text 
+#❞ text 
+#⊰ text 
+#˖˖ text
